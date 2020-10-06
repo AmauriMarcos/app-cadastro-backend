54547d0101301d43a11756cb02a9eb3f5346e2ae
AmauriMarcos <amaurisantos.m@gmail.com>
2020-10-06 10:33:23 +0200

    init

--- server.js
+++ server.js
@@ -24,16 +24,6 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'] */
 }));
 
-/* app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Headers', 
-        'Origin, X-Requested-Width, Content-Type, Accept, Authorization');
-    if(req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-        return res.status(200).json({});
-    }
-}); */
 
 app.use('/clientes', clientRoutes);
 
